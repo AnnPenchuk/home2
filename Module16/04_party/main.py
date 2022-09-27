f2b0be62353cee94f94469128390c93254415db0
Skillbox <it@skillbox.ru>
2022-09-27 22:55:40 +0300

    выполнено

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,22 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+count=len(guests)
+print('сейчас на вечеринке, ',count, 'человек:', guests)
+action = input('Гость пришёл или ушёл? ')
+while action!='пора спать':
+            if action=='пришел':
+                name=input('имя гостя')
+                сount = len(guests)
+                if count<6:
+                     print('привет, ',name)
+                     guests.append(name)
+                else:
+                     print('прости, ', name, ', но мест нет.')
+            elif action=='ушел':
+                 name = input('имя гостя')
+                 print('пока, ',name)
+                 guests.remove(name)
+            count = len(guests)
+            print('сейчас на вечеринке, ', count, 'человек:', guests)
+            action = input('Гость пришёл или ушёл? ')
+print('Вечеринка закончилась, все легли спать.')
