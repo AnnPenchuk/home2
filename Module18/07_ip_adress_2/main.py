c22d798bf3d0a7de238fd4d525f866dabf9ac4a8
Анна <annapenchuk95@mail.ru>
2022-10-16 03:04:29 +0300

    7 исправленоо

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,29 +1,29 @@
 IP=input('Введите IP: ')
 str=IP.split('.')
-count=0
+print(str)
+def is_good_len(str):
+    if len(str)!= 4:
+        print('aдрес - это четыре числа, разделённые точками')
+    return len(str)
 
-if len(str) < 4:
-    print('aдрес - это четыре числа, разделённые точками')
-    count+=1
-
-else:
+def is_int(str):
+    count= 0
     for i in str:
+        if not i.isdigit():
+               print(i, ' это не целое число.')
+               count += 1
+    return count
 
-       if i.isdigit():
+def is_range(str):
+    count1=0
+    for i in str:
+        if  int(i)<0:
+                print(i,'должно быть больше 0')
+                count1 += 1
+        if int(i) > 255:
+            print(i, 'должно быть меньше 255')
+            count1 += 1
+    return count1
 
-
-           if int(i)<0:
-                print(i,'меньше 0')
-                count += 1
-                break
-           elif int(i)>255:
-                print(i, 'больше 255')
-                count += 1
-                break
-
-
-       else:
-            print(i, ' это не целое число.')
-            count+=1
-if count<1:
+if is_good_len(str)== 4 and is_int(str)==0 and is_range(str)==0:
     print('IP-адрес корректен.')
